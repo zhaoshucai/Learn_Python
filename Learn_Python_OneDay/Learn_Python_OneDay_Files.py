5d68aa042940b7600b0928c8528567cc14bd6eb2
zhaoshucai <zhao.shucai@foxmail.com>
2019-06-23 14:48:17 +0700

    Learn_Python_OneDay_Files

--- Learn_Python_OneDay/Learn_Python_OneDay_Files.py
+++ Learn_Python_OneDay/Learn_Python_OneDay_Files.py
@@ -1,1 +1,8 @@
+# 随机产生IP
 import random
+sr1 = random.randint(1, 255)
+sr2 = random.randint(1, 255)
+sr3 = random.randint(1, 255)
+sr4 = random.randint(1, 255)
+random_ip = str(sr1)+"."+str(sr2)+"."+str(sr3)+"."+str(sr4)
+print(random_ip)
