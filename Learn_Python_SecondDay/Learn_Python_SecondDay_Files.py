9de49df4ab817f239508d5f0cc53990382e5fd0f
zhaoshucai <zhao.shucai@foxmail.com>
2019-06-24 20:38:09 +0700

    Learn_Python_SecondDay_Files

--- Learn_Python_SecondDay/Learn_Python_SecondDay_Files.py
+++ Learn_Python_SecondDay/Learn_Python_SecondDay_Files.py
@@ -40,4 +40,12 @@
 
 
 5
+import re
+str1 = 'Port-channel1.189        192.168.189.254    YES   CONFIG     up       up'
+result = re.match('\s*(\w.*\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s*',str1).groups()
+print(result)
+print('-'*80)
+print('%-7s:%s'%('接口',result[0]))
+print('%-7s:%s'%('IP地址',result[1]))
+print('%-7s:%s'%('状态',result[4]))
 
