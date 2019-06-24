9f8d7d02260a48d2c7a228a4d3e1d1c97fa7ff6d
zhaoshucai <zhao.shucai@foxmail.com>
2019-06-24 17:41:44 +0700

    Learn_Python_SecondDay_Files

--- Learn_Python_SecondDay/Learn_Python_SecondDay_Files.py
+++ Learn_Python_SecondDay/Learn_Python_SecondDay_Files.py
@@ -1,5 +1,5 @@
 # 1
-# print('QYTANG'+'’'+'day'+' '+'2014'+'-'+'9'+'-'+'28')
+# print('QYTANG'+'\''+'day'+'\000'+'2014'+'-'+'9'+'-'+'28')
 
 
 # 2
@@ -8,7 +8,36 @@
 # print(sub_word)
 
 
-3
-word = "Python"
-sub_word = word[1:6]+str('-')+word[0]+str('y')
-print(sub_word)
+# 3
+# word = "Python"
+# sub_word = word[1:6]+str('-')+word[0]+str('y')
+# print(sub_word)
+
+
+# 4
+# department1 = 'Security'
+# department2 = 'Python'
+# depart1_m = 'cq_bomb'
+# depart2_m = 'qinke'
+# COURSE_FEES_SEC = 456789.12456
+# COURSE_FEES_Python = 1234.3456
+#
+# line1 = 'Department1 Name:%-10s Manager:%-10s COURSE_FEES:%-10.2f The End!' % (department1,depart1_m,\
+#                                                                                COURSE_FEES_SEC)
+# line2 = 'Department2 Name:%-10s Manager:%-10s COURSE_FEES:%-10.2f The End!' % (department2,depart2_m,\
+#                                                                                COURSE_FEES_Python)
+#
+# line1 = 'Department1 Name:{0:<10} Manager:{1:<10} COURSE_FEES:{2:<10.2f} The End!'.format(department1,depart1_m,\
+#                                                                                          COURSE_FEES_SEC)
+# line2 = 'Department2 Name:{0:<10} Manager:{1:<10} COURSE_FEES:{2:<10.2f} The End!'.format(department2,depart2_m,\
+#                                                                                          COURSE_FEES_Python)
+#
+# length = len(line1)
+# print('='*length)
+# print(line1)
+# print(line2)
+# print('='*length)
+
+
+5
+
