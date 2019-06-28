dfbebd578787529fd7e44a86a87cac60420b3ae5
zhaoshucai <zhao.shucai@foxmail.com>
2019-06-28 22:58:19 +0700

    Learn_Python_SixthDay_Files

--- Learn_Python_SixthDay/Learn_Python_SixthDay_Files.py
+++ Learn_Python_SixthDay/Learn_Python_SixthDay_Files.py
@@ -4,6 +4,7 @@ asa_conn = "TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, idle 0
  bytes 74, flags UIO\n TCP Student 192.168.189.167:80 Teacher 137.78.5.128:65233, \
  idle 0:00:03, bytes 334516, flags UIO"
 asa_dict = {}
+
 for conn in asa_conn.split('\n')
     re_result = re.match()
     asa_dict[] = ()
@@ -20,11 +21,18 @@ format_str1 =
 format_str2 =
 print('\n格式化打印输出\n')
 for key, value in asa_dict.items():
-2
-port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23',\
-             'eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18',\
-             'eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
 
 
 
-8c1a092f692f.sn.mynetname.net:8292
+# 2
+# import re
+# port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23',\
+#              'eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18',\
+#              'eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+#
+# port_list.sort(key=lambda x: (int(re.findall(r'\d+', x)[0]),
+#                               int(re.findall(r'\d+', x)[1]),
+#                               int(re.findall(r'\d+', x)[2]),
+#                               int(re.findall(r'\d+', x)[3])))
+# print(port_list)
+
