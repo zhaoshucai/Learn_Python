1957e0e5ac717d7658bc35e9bbfdc741a66c2998
zhaoshucai <zhao.shucai@foxmail.com>
2019-07-15 21:24:31 +0700

    Learn_Python_FifteenDay_Files

---
+++ Learn_Python_FifteenDays/Learn_Python_FifteenDays_Files.py
@@ -0,0 +1,22 @@
+import sqlite3
+import os
+
+homework_dict = [{'姓名':'学员1','年龄':37,'作业数':1},
+                 {'姓名':'学员2','年龄':38,'作业数':9},
+                 {'姓名':'学员3','年龄':39,'作业数':7}]
+
+if os.path.exists('homework.sqlite'):
+    os.remove('homework_dict')
+
+conn = sqlite3.connect('homework_dict')
+cursor = conn.cursor()
+
+cursor.execute('create table homework_info(姓名 varchar(40),年龄 int,作业数 int)')
+
+for teacher in homework_dict:
+    name = teacher['姓名']
+    age = teacher['年龄']
+    honework = teacher['作业数']
+    cursor.execute('insert into homework_info values (
+
+conn.commit()
